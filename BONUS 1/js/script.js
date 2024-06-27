931c8bb1439e8b8d0bcafa519bbe49ac6cce12d1
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-27 16:15:06 +0200

    [BONUS1] COMPLETED

--- BONUS 1/js/script.js
+++ BONUS 1/js/script.js
@@ -7,17 +7,26 @@ for(let i = 1; i <= 100 ; i++){
     // CONTROLLO I NUMERI MULTIPLI DI 3 E 5
     if (i % 3 == 0 && i % 5 == 0){
         console.log("FrizzBuzz");
+        // INSERISCO IL TESTO ALL'INTERNO DEL ELEMENTO CREATO
+        content.innerText = `FrizzBuzz`;
     }
     // CONTROLLO I NUMERI MULTIPLI DI 3
      else if(i % 3 == 0){
         console.log("Frizz")
+        // INSERISCO IL TESTO ALL'INTERNO DEL ELEMENTO CREATO
+        content.innerText = `Frizz`;
     }
     // CONTROLLO I NUMERI MULTIPLI DI 5
     else if(i % 5 == 0){
         console.log("Buzz")
+        // INSERISCO IL TESTO ALL'INTERNO DEL ELEMENTO CREATO
+        content.innerText = `Buzz`;
     } 
     else{
         console.log(i)
+        // INSERISCO IL TESTO ALL'INTERNO DEL ELEMENTO CREATO
+        content.innerText = `${i}`;
     }
-
+    // APPENDO GLI ELEMENTI CREATI 
+    container.append(content);
 }
