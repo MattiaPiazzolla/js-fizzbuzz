bd4c625f2d26bb6b0e47da9b351f881968c241c2
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-27 15:50:43 +0200

    [BONUS 1] get container id to the const container

---
+++ BONUS 1/js/script.js
@@ -0,0 +1,21 @@
+// RECUPERO IL CONTAINER DELL'HTML
+const container = document.getElementById("container")
+// CICLO I NUMERI DA 1 A 100
+for(let i = 1; i <= 100 ; i++){
+    // CONTROLLO I NUMERI MULTIPLI DI 3 E 5
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FrizzBuzz");
+    }
+    // CONTROLLO I NUMERI MULTIPLI DI 3
+     else if(i % 3 == 0){
+        console.log("Frizz")
+    }
+    // CONTROLLO I NUMERI MULTIPLI DI 5
+    else if(i % 5 == 0){
+        console.log("Buzz")
+    } 
+    else{
+        console.log(i)
+    }
+
+}
