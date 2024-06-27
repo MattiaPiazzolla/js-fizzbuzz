a8d2269bd4e78e462355a50fcc5f6866ead6905b
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-27 15:31:17 +0200

    [UPD] check multiple numbers of 3 and 5 and print frizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 // CICLO I NUMERI DA 1 A 100
 for(let i = 1; i <= 100 ; i++){
+    // CONTROLLO I NUMERI MULTIPLI DI 3 E 5
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FrizzBuzz");
+    }
+    
     console.log(i)
+
 }
