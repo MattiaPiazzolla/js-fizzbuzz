36ebe9969e3f75b4d741e385b1207569fde99cc6
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-27 15:59:39 +0200

    [BONUS1] append element created

--- BONUS 1/js/script.js
+++ BONUS 1/js/script.js
@@ -1,7 +1,9 @@
 // RECUPERO IL CONTAINER DELL'HTML
-const container = document.getElementById("container")
+const container = document.getElementById("container");
 // CICLO I NUMERI DA 1 A 100
 for(let i = 1; i <= 100 ; i++){
+    // CREO UN ELEMENTO DA APPENDERE 
+    const content = document.createElement('div')
     // CONTROLLO I NUMERI MULTIPLI DI 3 E 5
     if (i % 3 == 0 && i % 5 == 0){
         console.log("FrizzBuzz");
